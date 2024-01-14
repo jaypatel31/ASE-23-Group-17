30c5e43771562932593c7ea7e1c89abfada90aef
Jay Patel <jaypatel32157@gmail.com>
2024-01-14 11:11:30 -0500

    Num Test Seperated

--- hw/w2/src/num_test.py
+++ hw/w2/src/num_test.py
@@ -6,18 +6,21 @@ class TestNUM(unittest.TestCase):
     def setUp(self):
         # Create an instance of NUM for testing
         self.num_instance = NUM()
-
-    def test_add_multiple_values(self):
-        # Test adding multiple numeric values
-        values = [3, 7, 5, 8, 2]
-        for value in values:
+        self.values = [3, 7, 5, 8, 2]
+        for value in self.values:
             self.num_instance.add(value)
 
-        self.assertEqual(self.num_instance.n, len(values))
-        self.assertAlmostEqual(self.num_instance.mu, sum(values) / len(values), delta=1e-10)
-        self.assertAlmostEqual(self.num_instance.m2, sum((x - self.num_instance.mu) ** 2 for x in values), delta=1e-10)
-        self.assertEqual(self.num_instance.lo, min(values))
-        self.assertEqual(self.num_instance.hi, max(values))
+    def test_num_add(self):
+        # Test checking the addition of new number
+        self.assertEqual(self.num_instance.n, len(self.values)) 
+    
+    def test_num_stats(self):
+        # Test Checking Stats of Values
+
+        self.assertAlmostEqual(self.num_instance.mu, sum(self.values) / len(self.values), delta=1e-10)
+        self.assertAlmostEqual(self.num_instance.m2, sum((x - self.num_instance.mu) ** 2 for x in self.values), delta=1e-10)
+        self.assertEqual(self.num_instance.lo, min(self.values))
+        self.assertEqual(self.num_instance.hi, max(self.values))
 
 if __name__ == '__main__':
     unittest.main()
