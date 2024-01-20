451aff0a628eeae1065474fd58f13078950c9bc6
shail611 <shailshah1106@gmail.com>
2024-01-20 12:19:49 -0500

    Added likes

--- hw/w3/src/ROW.py
+++ hw/w3/src/ROW.py
@@ -7,7 +7,7 @@ class ROW:
     def __iter__(self):
         return iter(self.cells)
     
-    def row_like(self, data, n, nHypotheses, prior, out, v, inc):
+    def like(self, data, n, nHypotheses, prior, out, v, inc):
         prior = (len(data.rows) + the['k']) / (n + the['k'] * nHypotheses)
         out = math.log(prior)
         for col in data.cols.x.values():
@@ -16,4 +16,18 @@ class ROW:
                 inc = col.like(v, prior)
                 out += math.log(inc)
         return math.exp(1) ** out
+    
+    def likes(self, datas, n, nHypotheses, most, tmp, out):
+        n, nHypotheses = 0, 0
+        for _, data in datas.items():
+            n += len(data.rows)
+            nHypotheses += 1
+
+        for _, data in datas.items():
+            tmp = self.row_like(data, n, nHypotheses)
+            if most is None or tmp > most:
+                most, out = tmp, _
+                
+        return out, most
+
 
