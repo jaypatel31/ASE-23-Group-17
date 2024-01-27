0a4e1f0e558716749dfb9346d7bd2b49423e007c
Soham Bapat <47332484+sohambapat@users.noreply.github.com>
2024-01-27 12:32:56 -0500

    Create DATA.py
    added bestrest

---
+++ hw/w4/src/DATA.py
@@ -0,0 +1,47 @@
+from ROW import ROW
+from COLS import COLS
+
+class DATA:
+    def __init__(self, src, fun=None):
+        self.rows = []
+        self.cols = None
+
+    def add(self, t, fun=None):
+        row = t if isinstance(t, ROW) else ROW(t)
+        if self.cols:
+            if fun:
+                fun(self, row)
+            self.rows.append(self.cols.add(row))
+        else:
+            self.cols = COLS(row)  
+
+    # The following methods need the implementation of mid, div, small, and stats 
+    # methods in the respective classes they are called on
+    def mid(self, cols=None):
+        u = [col.mid() for col in (cols or self.cols.all)]
+        return ROW(u)
+
+    def div(self, cols=None):
+        u = [col.div() for col in (cols or self.cols.all)]
+        return ROW(u)
+
+    def small(self):
+        u = [col.small() for col in self.cols.all]
+        return ROW(u)
+
+    def stats(self, cols=None, fun=None, ndivs=2):
+        u = {".N": len(self.rows)}
+        col_name = cols if cols else self.cols.all
+        for col in (col_name):
+            u[col.txt] = round(float(col.mid()), ndivs) if isinstance(col.mid(), (int, float)) else col.mid()
+        return u
+
+    def best_rest(self, rows, want, best, rest, top):
+            rows.sort(key=lambda row: row.d2h(self))
+            best, rest = [self.cols['names']], [self.cols['names']]
+            for i, row in enumerate(rows):
+                if i < want:
+                    best.append(row)
+                else:
+                    rest.append(row)
+            return  DATA.new(best), DATA.new(rest)
