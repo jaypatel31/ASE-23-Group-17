c567436561056e627d3d22f1a44785469de92d46
shail611 <shailshah1106@gmail.com>
2024-02-18 01:20:57 -0500

    Update NODE.py

--- hw/w6/src/NODE.py
+++ hw/w6/src/NODE.py
@@ -21,21 +21,17 @@ class NODE:
     def d2h(self, data):
             return self.rnd(data.mid().d2h(self.here))
     
-    def show(self, _show, maxDepth):
-
-        maxDepth = 0
-
-        def _show(node, depth, leafp):
-            nonlocal maxDepth
-            # print(type(ROW.d2h(node.here)),type(node.here.mid().cells))
-            post = str(self.d2h(node.here).here.rows[leafp]) + "\t" + str(node.here.mid().cells) if leafp else ""
-            maxDepth = max(maxDepth, depth)
-            print(('|.. ' * (depth+1)) + post)
+    def show(self, max_depth=0):
+        def _show(node, depth, is_leaf):
+            post = ""
+            if is_leaf:
+                d2h_value = self.rnd(node.here.mid().d2h(self.here))
+                post = f"{d2h_value}\t{str(node.here.mid().cells)}"
+            nonlocal max_depth
+            max_depth = max(max_depth, depth)
+            print('|.. ' * depth + post)
 
         self.walk(_show)
-        print("there")
-        print("    " * maxDepth + str(self.d2h(self.here).here.rows[0].cells), self.here.mid().cells)
-        print("    " * maxDepth + "_", self.here.cols.names)
-        print("here")
-
-
+        print("")
+        print("    " * max_depth, self.rnd(self.here.mid().d2h(self.here)), str(self.here.mid().cells))
+        print("    " * max_depth, "_", str(self.here.cols.names))
