1021dafc32e090f66757dd05b0e60a86a78a520a
Jay Patel <jaypatel32157@gmail.com>
2024-04-10 14:37:32 -0400

    some fix

---
+++ hw/final/src/RULE.py
@@ -0,0 +1,72 @@
+class RULE:
+    def __init__(self, ranges):
+        self.parts = {}
+        self.scored = 0
+        for range_ in ranges:
+            t = self.parts.get(range_.txt, [])
+            t.append(range_)
+            self.parts[range_.txt] = t
+
+    def _or(self, ranges, row):
+        x = row.cells[ranges[0].at]
+        if x == "?":
+            return True
+        for range_ in ranges:
+            lo, hi = range_.x['lo'], range_.x['hi']
+            if lo == hi and lo == x or lo <= x < hi:
+                return True
+        return False
+
+    def _and(self, row):
+        for ranges in self.parts.values():
+            if not self._or(ranges, row):
+                return False
+        return True
+
+    def selects(self, rows):
+        t = []
+        for row in rows:
+            if self._and(row):
+                t.append(row)
+        return t
+
+    def selectss(self, rowss):
+        t = {}
+        for y, rows in rowss.items():
+            t[y] = len(self.selects(rows))
+        return t
+
+    def show(self):
+        ands = []
+        for ranges in self.parts.values():
+            ors = _showLess(ranges)
+            at = ors[0].at
+            ors = [range_.show() for range_ in ors]
+            ands.append(" or ".join(ors))
+        return " and ".join(ands)
+
+def _showLess(t, ready=False):
+    if not ready:
+        t = copy(t)  # important, since we are about to mess up the y counts
+        t.sort(key=lambda a: a.x['lo'])
+
+    i, u = 0, []
+    while i < len(t):
+        a = t[i]
+        if i < len(t) - 1:
+            if a.x['hi'] == t[i + 1].x['lo']:
+                a = a.merge(t[i + 1])  # warning. the y counts now may be very wrong
+                i += 1
+        u.append(a)
+        i += 1
+
+    return t if len(u) == len(t) else _showLess(u, True)
+
+def copy(t):
+    if not isinstance(t, dict):
+        return t
+    u = {}
+    for k, v in t.items():
+        u[copy(k)] = copy(v)
+    return u
+
---
+++ hw/final/src/RULES.py
@@ -0,0 +1,62 @@
+from config import the
+from RULE import RULE
+
+class RULES:
+    def __init__(self, ranges, goal, rowss):
+        self.sorted = []
+        self.goal = goal
+        self.rowss = rowss
+        self.LIKE = 0
+        self.HATE = 0
+        self.likeHate()
+        for range_ in ranges:
+            range_.scored = self.score(range_.y)
+        self.sorted = self.top(self.trys(self.top(ranges)))
+
+    def likeHate(self):
+        for y, rows in self.rowss.items():
+            if y == self.goal:
+                self.LIKE += len(rows)
+            else:
+                self.HATE += len(rows)
+
+    def score(self, t):
+        return score(t, self.goal, self.LIKE, self.HATE)
+
+    def trys(self, ranges):
+        u = []
+        for subset in powerset(ranges):
+            if subset:
+                rule = RULE(subset)
+                rule.scored = self.score(rule.selectss(self.rowss))
+                if rule.scored > 0.01:
+                    u.append(rule)
+        return u
+
+    def top(self, t):
+        t.sort(key=lambda x: x.scored, reverse=True)
+        u = []
+        for x in t:
+            if x.scored >= t[0].scored * the.Cut:
+                u.append(x)
+        return u
+
+def powerset(s):
+    t = [[]]
+    for i in range(len(s)):
+        for j in range(len(t)):
+            t.append([s[i]] + t[j])
+    return t
+
+def score(t, goal, LIKE, HATE, like=None, hate=None, tiny=None):
+    like, hate, tiny = 0, 0, 1E-30
+    for klass, n in t.items():
+        if klass == goal:
+            like += n
+        else:
+            hate += n
+    like, hate = like / (LIKE + tiny), hate / (HATE + tiny)
+    if hate >= like:
+        return 0
+    else:
+        return like ** the.Support / (like + hate)
